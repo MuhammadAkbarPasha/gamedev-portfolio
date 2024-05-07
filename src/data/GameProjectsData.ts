6bdbc37fb704e80ce20412d948de3f9182990cf2
Muhammad Akbar <32601960+MuhammadAkbarPasha@users.noreply.github.com>
2024-05-07 15:15:51 +0500

    Updates

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -7,8 +7,7 @@ export default [
     "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
     `
     <div class="paragraph">
-        <strong>Stardust League</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-        <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+        <strong>Stardust League</strong> is a pvp anime RPG game, inspired from Genshin Impact. The game had lobby, team battles, Gacha,equipment features and bunch of character functionalities and fighting styles.
     </div>
     <div class="paragraph center">
         <video class="project-video" controls autoplay loop>
@@ -17,23 +16,26 @@ export default [
         </video>
     </div>
     <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+        <a href="https://store.steampowered.com/app/2134680/Stardust_League/" target="_blank"><img src="img/projects/steambadge.png" width="250" height="90" alt="Play Store badge" /></a>
     </div>
 
     <div class="paragraph">
-        Main features :
+   I joined the team of this game in middle of the production cycle, and worked for 7 months on it.  
+       <br> Features I contributed in :
         <ul>
-            <li>Some stuff</li>
-            <li>Some great stuff</li>
-            <li>More awesome stuff</li>
-            <li>And then some</li>
+            <li>Server and client side logic and setup for teaming, new modes, maps, spectate, win and lose</li>
+            <li>Socket based system on both client and server for features such as chat, teaming and other event based features</li>
+            <li>Gacha, Settings, Achievements System, Equipment system and UI </li>
+            <li>Virtual Economy/Currency</li>
         </ul>
     </div>
 
-    <div class="paragraph center">
+   <!-- <div class="paragraph center">
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
         <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
     </div>
+
+    -->
     `,
     "#23bd69",
     true,true
@@ -42,10 +44,11 @@ export default [
 "img/projects/Turret.jpg",
 "videos/Turret.mp4", // Assuming the video file is in the 'videos' folder
 `
+
 <div class="paragraph">
-    <strong>Turret Game</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-    <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
-</div>
+        <strong>Turret Game</strong> inspired from Survivor.io, is a 2d top down game, where player gets to control the rotation of the center turret to shoot down enemy zombies coming from all 
+        directions. During the gameplay, the player can also equip spell cards to activate abilities like meteor shower, air push, drones etc. The game has bunch of meta features for player retention and rewards them for playing the game.  
+    </div>
 <div class="paragraph center">
     <video class="project-video" controls autoplay loop>
         <source src="videos/Turret.mp4" type="video/mp4">
@@ -53,23 +56,26 @@ export default [
     </video>
 </div>
 <div class="paragraph center">
-    <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-</div>
 
-<div class="paragraph">
-    Main features :
-    <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-    </ul>
 </div>
 
 <div class="paragraph center">
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+In development right now
+
 </div>
+<div class="paragraph">
+I was completely responsible for this game, from ideation to implementation. In the development I have tried my best to adapt best practices for both personal learning and game optimization.
+ Features of this game can be exported to any other game.
+    <br> Features I contributed in :
+     <ul>
+         <li>Controller, shooting mechanism, Enemy AIs, Boss AIs, Choice Functionality, Spell cards</li>
+         <li>Weapon stats, weapon stat upgrades, in game player stats and upgrades, gameplay continuity and enemy spawning</li>
+         <li>Meta features : Dailies, Weeklies, Achievements, Gacha, Equipment, Equipment upgrades and rarities, Merge, Shop, Character Ability Tree, Level Maps </li>
+         <li>Monetization : Shop, Monthly Subscription Cards, Battle Pass</li>
+     </ul>
+ </div>
+
+
 `,
 "#FA01DC",
 true
@@ -79,8 +85,7 @@ true
 "videos/Differences.mp4", // Assuming the video file is in the 'videos' folder
 `
 <div class="paragraph">
-    <strong>Differences</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-    <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+    <strong>Differences</strong> is 2d puzzle game where player has to find the differences or find the objects from given image(s).
 </div>
 <div class="paragraph center">
     <video class="project-video" controls autoplay loop>
@@ -89,23 +94,21 @@ true
     </video>
 </div>
 <div class="paragraph center">
-    <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+Not available on any store
 </div>
 
 <div class="paragraph">
-    Main features :
-    <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-    </ul>
+Features I contributed in :
+<ul>
+    <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
+    <li>Meta features : Leaderboards, managing of player data using Playfab</li>
+    <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
+    <li>Other features : Game Currency and dynamic shop using playfab </li>
+   
+</ul>
 </div>
 
-<div class="paragraph center">
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-</div>
+
 `,
 "#F4052D",
 true
@@ -115,8 +118,7 @@ true
 "videos/Murasaki.mp4", // Assuming the video file is in the 'videos' folder
 `
 <div class="paragraph">
-    <strong>Murasaki 7</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-    <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+    <strong>Murasaki 7</strong> is an anime style match puzzle RPG game.
 </div>
 <div class="paragraph center">
     <video class="project-video" controls autoplay loop>
@@ -125,23 +127,23 @@ true
     </video>
 </div>
 <div class="paragraph center">
-    <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+
+<a href=" https://www.youtube.com/watch?v=B_frwQteTls&t=546s&ab_channel=NoXModel001" target="_blank">Watch Gameplay on Youtube</a>
+
 </div>
 
 <div class="paragraph">
-    Main features :
-    <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-    </ul>
+Features I contributed in :
+<ul>
+    <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
+    <li>Meta features : Leaderboards, managing of player data using Playfab</li>
+    <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
+    <li>Other features : Game Currency and dynamic shop using playfab </li>
+   
+</ul>
 </div>
 
-<div class="paragraph center">
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-</div>
+
 `,
 "#0198FA",
 true
@@ -151,8 +153,9 @@ true
 "videos/Plane Game.mp4", // Assuming the video file is in the 'videos' folder
 `
 <div class="paragraph">
-    <strong>Pilot Flight Simulator</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-    <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+    <strong>Pilot Flight Simulator</strong> is a simulation game with a bunch of modes and missions to play from, not proud of it anymore because now it has a bunch of ads 🤦🏼‍♂️🤦🏼‍♂️😂😂.
+
+
 </div>
 <div class="paragraph center">
     <video class="project-video" controls autoplay loop>
@@ -161,23 +164,19 @@ true
     </video>
 </div>
 <div class="paragraph center">
-    <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+    <a href="https://play.google.com/store/apps/details?id=com.gp.pilot.simulator.airplanegames&hl=en-US&gl=US" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
 </div>
 
 <div class="paragraph">
-    Main features :
+I worked on this game from ideation to first launch and some updates.<br>  
+Features I added : 
     <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Controller, Waypoint system, Win/Lose functionality, Map, Modes and Missions </li>
+        <li>IAPs, Plane Selection, Mission Selection, Upgrades</li>
+        <li>Settings, Game day cycle(minor stuff) and Ads</li>
     </ul>
 </div>
 
-<div class="paragraph center">
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-</div>
 `,
 "#0198FA",
 true   ,
