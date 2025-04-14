45c72f4dce065bbdd3829f38d6fb30d3cb47ac37
MuhammadAkbarPasha <muhammadakbar13995@gmail.com>
2025-04-14 17:46:05 +0500

    work

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,11 +1,122 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
+
+
+    new ProjectData("Turret Game",
+        "Turret Game",
+        "img/projects/Turret.jpg",
+        "videos/Turret.mp4", // Assuming the video file is in the 'videos' folder
+        `
+    
+    <div class="paragraph">
+            <strong>Turret Game</strong> inspired from Survivor.io, is a 2d top down game, where player gets to control the rotation of the center turret to shoot down enemy zombies coming from all 
+            directions. During the gameplay, the player can also equip spell cards to activate abilities like meteor shower, air push, drones etc. The game has bunch of meta features for player retention and rewards them for playing the game.  
+        </div>
+    <div class="paragraph center">
+        <video class="project-video" controls autoplay loop>
+            <source src="videos/Turret.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="paragraph center">
+    
+    </div>
+    
+    <div class="paragraph center">
+    In development right now
+    
+    </div>
+    <div class="paragraph">
+    I was completely responsible for this game, from ideation to implementation. In the development I have tried my best to adapt best practices for both personal learning and game optimization.
+     Features of this game can be exported to any other game.
+        <br> Features I contributed in :
+         <ul>
+             <li>Controller, shooting mechanism, Enemy AIs, Boss AIs, Choice Functionality, Spell cards</li>
+             <li>Weapon stats, weapon stat upgrades, in game player stats and upgrades, gameplay continuity and enemy spawning</li>
+             <li>Meta features : Dailies, Weeklies, Achievements, Gacha, Equipment, Equipment upgrades and rarities, Merge, Shop, Character Ability Tree, Level Maps </li>
+             <li>Monetization : Shop, Monthly Subscription Cards, Battle Pass</li>
+         </ul>
+     </div>
+    
+    
+    `,
+        "#FA01DC",
+        true
+    ), new ProjectData("Differences",
+        "Differences",
+        "img/projects/Differences.png",
+        "videos/Differences.mp4", // Assuming the video file is in the 'videos' folder
+        `
+    <div class="paragraph">
+        <strong>Differences</strong> is 2d puzzle game where player has to find the differences or find the objects from given image(s).
+    </div>
+    <div class="paragraph center">
+        <video class="project-video" controls autoplay loop>
+            <source src="videos/Differences.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="paragraph center">
+    Not available on any store
+    </div>
+    
+    <div class="paragraph">
+    Features I contributed in :
+    <ul>
+        <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
+        <li>Meta features : Leaderboards, managing of player data using Playfab</li>
+        <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
+        <li>Other features : Game Currency and dynamic shop using playfab </li>
+       
+    </ul>
+    </div>
+    
+    
+    `,
+        "#F4052D",
+        true
+    ), new ProjectData("Murasaki 7",
+        "Murasaki 7",
+        "img/projects/Murasaki.png",
+        "videos/Murasaki.mp4", // Assuming the video file is in the 'videos' folder
+        `
+    <div class="paragraph">
+        <strong>Murasaki 7</strong> is an anime style match puzzle RPG game.
+    </div>
+    <div class="paragraph center">
+        <video class="project-video" controls autoplay loop>
+            <source src="videos/Murasaki.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="paragraph center">
+    
+    <a href=" https://www.youtube.com/watch?v=B_frwQteTls&t=546s&ab_channel=NoXModel001" target="_blank">Watch Gameplay on Youtube</a>
+    
+    </div>
+    
+    <div class="paragraph">
+    Features I contributed in :
+    <ul>
+        <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
+        <li>Meta features : Leaderboards, managing of player data using Playfab</li>
+        <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
+        <li>Other features : Game Currency and dynamic shop using playfab </li>
+       
+    </ul>
+    </div>
+    
+    
+    `,
+        "#0198FA",
+        true
+    ),
     new ProjectData("AI Layer MOBA",
-    "AI Layer MOBA",
-    "img/projects/AILayerMoba.jpg",
-    "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
-    `
+        "AI Layer MOBA",
+        "img/projects/AILayerMoba.jpg",
+        "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
+        `
     <div class="paragraph">
         <strong>AI Layer MOBA</strong> is a MOBA Cyberpunk styled game with AI creeps, Turrets and base. The gameplay consists of 2v2 with creeps spawning after every few minutes
         and the goal is to destroy other team's turret and base.  </div>
@@ -35,15 +146,86 @@ export default [
 
     -->
     `,
-    "#23bd69",
-    true,true
-),
+        "#23bd69",
+        true, true
+    ),
 
-new ProjectData("Stardust League",
-    "Stardust League",
-    "img/projects/StardustLeague.jpg",
-    "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
-    `
+
+
+    new ProjectData("FPS Game Using Unreal",
+        "FPS Game Using Unreal",
+        "img/projects/wagmi.png",
+        "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
+        `
+    <div class="paragraph">
+    <strong>Overview:</strong>
+    <p>
+        This game prototype is inspired by Overwatch 2, featuring a fast-paced, action-packed first-person shooter experience. Built in Unreal Engine, the project showcases advanced gameplay mechanics, unique abilities, and a polished user interface tailored for immersive combat.
+    </p>
+</div>
+    <div class="paragraph center">
+       <div style="text-align: center;">
+    <iframe class="project-video-center" width="560" height="315"
+        src="https://www.youtube.com/embed/CD9bGARsrBg?autoplay=1&loop=1&playlist=CD9bGARsrBg"
+        title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+    </iframe>
+</div>
+
+    </div>
+   
+
+ 
+
+<div class="paragraph">
+    <strong>Key Features:</strong>
+    <ul>
+        <li>
+            <strong>Combat System:</strong>
+            <ul>
+                <li>Basic attack mechanics allowing players to deal damage to dummies.</li>
+                <li>Three dynamic abilities:
+                    <ul>
+                        <li><strong>Laser Attack:</strong> A precise, high-energy beam for focused damage.</li>
+                        <li><strong>Grenade Throw:</strong> An explosive projectile for area damage.</li>
+                        <li><strong>Upward Dash:</strong> A mobility skill for quick escapes or strategic positioning.</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <strong>Ultimate Ability:</strong>
+            <ul>
+                <li>Transforms the character to a third-person view, unleashing a barrage of devastating lasers, amplifying combat intensity.</li>
+            </ul>
+        </li>
+        <li>
+            <strong>Jetpack Functionality:</strong>
+            <ul>
+                <li>Vertical traversal with fluid jetpack controls, enhancing mobility and strategy.</li>
+            </ul>
+        </li>
+        <li>
+            <strong>User Interface & Controls:</strong>
+            <ul>
+                <li>Intuitive and responsive User Interface (UI) designed for smooth gameplay.</li>
+                <li>Fully implemented gamepad key mapping, allowing players to seamlessly navigate and activate abilities.</li>
+            </ul>
+        </li>
+    </ul>
+</div>
+
+    `,
+        "#23bd69",
+        true, true
+    ),
+
+    new ProjectData("Stardust League",
+        "Stardust League",
+        "img/projects/StardustLeague.jpg",
+        "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
+        `
     <div class="paragraph">
         <strong>Stardust League</strong> is a pvp anime RPG game, inspired from Genshin Impact. The game had lobby, team battles, Gacha,equipment features and bunch of character functionalities and fighting styles.
     </div>
@@ -75,164 +257,56 @@ new ProjectData("Stardust League",
 
     -->
     `,
-    "#23bd69",
-    true,true
-)
-,
+        "#23bd69",
+        true, true
+    )
 
 
-new ProjectData("Gnome Wars",
-    "Gnome Wars",
-    "img/projects/GnomeWars.png",
-    "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
-    `
-    <div class="paragraph">
-        <strong>Gnome Wars</strong>Gnome Wars is a fast-paced multiplayer FPS built in Unity and powered by the Mirror networking engine. 
-    </div>
-    <div class="paragraph center">
-        <video class="project-video" controls autoplay loop>
-            <source src="videos/GnomeWars.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
-    </div>
-    <div class="paragraph center">
-        <a href="https://store.steampowered.com/app/2134680/Stardust_League/" target="_blank"><img src="img/projects/steambadge.png" width="250" height="90" alt="Play Store badge" /></a>
-    </div>
+    /*
+        new ProjectData("Gnome Wars",
+            "Gnome Wars",
+            "img/projects/GnomeWars.png",
+            "videos/Stardust League.mp4", // Assuming the video file is in the 'videos' folder
+            `
+        <div class="paragraph">
+            <strong>Gnome Wars</strong>Gnome Wars is a fast-paced multiplayer FPS built in Unity and powered by the Mirror networking engine. 
+        </div>
+        <div class="paragraph center">
+            <video class="project-video" controls autoplay loop>
+                <source src="videos/GnomeWars.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+        <div class="paragraph center">
+            <a href="https://store.steampowered.com/app/2134680/Stardust_League/" target="_blank"><img src="img/projects/steambadge.png" width="250" height="90" alt="Play Store badge" /></a>
+        </div>
+    
+        <div class="paragraph">
+      Designed for WebGL, the game offers seamless browser-based play with smooth integration of CrazyGames for ad support. 
+      Players can log in to create or join private rooms, battle it out in a gnome-themed world, and access a dynamic in-game economy. 
+      The game includes a fully developed shop system, allowing players to purchase unique skins and weapons, with multiple choices tailored to different playstyles.
+    
+    Built on a Node.js backend, Gnome Wars ensures a stable multiplayer experience with custom matchmaking. With a blend of lighthearted characters and intense combat, players take control of gnomes and engage in thrilling battles, offering a fresh and quirky take on FPS gameplay. Whether customizing your gnome with skins or unlocking new weapons, Gnome Wars delivers a unique and fun experience for every player.
+    
+    
+        </div>
+    
+       <!-- <div class="paragraph center">
+            <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+            <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
+        </div>
+    
+        -->
+        `,
+            "#23bd69",
+            true, true
+        )*/
 
-    <div class="paragraph">
-  Designed for WebGL, the game offers seamless browser-based play with smooth integration of CrazyGames for ad support. 
-  Players can log in to create or join private rooms, battle it out in a gnome-themed world, and access a dynamic in-game economy. 
-  The game includes a fully developed shop system, allowing players to purchase unique skins and weapons, with multiple choices tailored to different playstyles.
-
-Built on a Node.js backend, Gnome Wars ensures a stable multiplayer experience with custom matchmaking. With a blend of lighthearted characters and intense combat, players take control of gnomes and engage in thrilling battles, offering a fresh and quirky take on FPS gameplay. Whether customizing your gnome with skins or unlocking new weapons, Gnome Wars delivers a unique and fun experience for every player.
-
-
-    </div>
-
-   <!-- <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    </div>
-
-    -->
-    `,
-    "#23bd69",
-    true,true
-)
-,
-new ProjectData("Turret Game",
-"Turret Game",
-"img/projects/Turret.jpg",
-"videos/Turret.mp4", // Assuming the video file is in the 'videos' folder
-`
-
-<div class="paragraph">
-        <strong>Turret Game</strong> inspired from Survivor.io, is a 2d top down game, where player gets to control the rotation of the center turret to shoot down enemy zombies coming from all 
-        directions. During the gameplay, the player can also equip spell cards to activate abilities like meteor shower, air push, drones etc. The game has bunch of meta features for player retention and rewards them for playing the game.  
-    </div>
-<div class="paragraph center">
-    <video class="project-video" controls autoplay loop>
-        <source src="videos/Turret.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-</div>
-<div class="paragraph center">
-
-</div>
-
-<div class="paragraph center">
-In development right now
-
-</div>
-<div class="paragraph">
-I was completely responsible for this game, from ideation to implementation. In the development I have tried my best to adapt best practices for both personal learning and game optimization.
- Features of this game can be exported to any other game.
-    <br> Features I contributed in :
-     <ul>
-         <li>Controller, shooting mechanism, Enemy AIs, Boss AIs, Choice Functionality, Spell cards</li>
-         <li>Weapon stats, weapon stat upgrades, in game player stats and upgrades, gameplay continuity and enemy spawning</li>
-         <li>Meta features : Dailies, Weeklies, Achievements, Gacha, Equipment, Equipment upgrades and rarities, Merge, Shop, Character Ability Tree, Level Maps </li>
-         <li>Monetization : Shop, Monthly Subscription Cards, Battle Pass</li>
-     </ul>
- </div>
-
-
-`,
-"#FA01DC",
-true
-),new ProjectData("Differences",
-"Differences",
-"img/projects/Differences.png",
-"videos/Differences.mp4", // Assuming the video file is in the 'videos' folder
-`
-<div class="paragraph">
-    <strong>Differences</strong> is 2d puzzle game where player has to find the differences or find the objects from given image(s).
-</div>
-<div class="paragraph center">
-    <video class="project-video" controls autoplay loop>
-        <source src="videos/Differences.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-</div>
-<div class="paragraph center">
-Not available on any store
-</div>
-
-<div class="paragraph">
-Features I contributed in :
-<ul>
-    <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
-    <li>Meta features : Leaderboards, managing of player data using Playfab</li>
-    <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
-    <li>Other features : Game Currency and dynamic shop using playfab </li>
-   
-</ul>
-</div>
-
-
-`,
-"#F4052D",
-true
-),new ProjectData("Murasaki 7",
-"Murasaki 7",
-"img/projects/Murasaki.png",
-"videos/Murasaki.mp4", // Assuming the video file is in the 'videos' folder
-`
-<div class="paragraph">
-    <strong>Murasaki 7</strong> is an anime style match puzzle RPG game.
-</div>
-<div class="paragraph center">
-    <video class="project-video" controls autoplay loop>
-        <source src="videos/Murasaki.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
-</div>
-<div class="paragraph center">
-
-<a href=" https://www.youtube.com/watch?v=B_frwQteTls&t=546s&ab_channel=NoXModel001" target="_blank">Watch Gameplay on Youtube</a>
-
-</div>
-
-<div class="paragraph">
-Features I contributed in :
-<ul>
-    <li>Full Multiplayer : 2 players can play against each other, first to find all differences from timer ends wins</li>
-    <li>Meta features : Leaderboards, managing of player data using Playfab</li>
-    <li>Community features : Clan, clan invite, friends, clan leader and sub leader, chat </li>
-    <li>Other features : Game Currency and dynamic shop using playfab </li>
-   
-</ul>
-</div>
-
-
-`,
-"#0198FA",
-true
-),new ProjectData("Pilot Flight Simulator",
-"Pilot Flight Simulator",
-"img/projects/PlaneGame.png",
-"videos/Plane Game.mp4", // Assuming the video file is in the 'videos' folder
-`
+    , new ProjectData("Pilot Flight Simulator",
+        "Pilot Flight Simulator",
+        "img/projects/PlaneGame.png",
+        "videos/Plane Game.mp4", // Assuming the video file is in the 'videos' folder
+        `
 <div class="paragraph">
     <strong>Pilot Flight Simulator</strong> is a simulation game with a bunch of modes and missions to play from, not proud of it anymore because now it has a bunch of ads 🤦🏼‍♂️🤦🏼‍♂️😂😂.
 
@@ -259,8 +333,8 @@ Features I added :
 </div>
 
 `,
-"#0198FA",
-true   ,
-true
-)
+        "#0198FA",
+        true,
+        true
+    )
 ];
